b44ec3f997bd6612747ef1c416d592ea051408f2
Ishtmeet Singh <ish.rissam@gmail.com>
2018-02-26 22:14:55 +0530

    added output file and changed getInput function

--- convert_num_to_string/main.cpp
+++ convert_num_to_string/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 
-int getInput(void) {
+long getLongInput(void) {
     std::cout << "Please enter a number between 0 and 2 billion " << std::endl;
     long input;
     std::cin >> input;
     return static_cast<long>(input);
 }
 
-int main(void){
-    long input = getInput();
+// Function to calculate number of characters and f
 
-   /*
-    Work under progress
-    */
+int main(void){
+    long input = getLongInput();
+    std::string number_to_string = std::to_string(input);
+
 
 
     return 0;
