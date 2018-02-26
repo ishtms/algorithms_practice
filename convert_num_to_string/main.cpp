e37828bbd55db0e5c9a671ace1854c09160c0dae
Ishtmeet Singh <ish.rissam@gmail.com>
2018-02-26 22:30:53 +0530

    added function to send the length to the main funciton

--- convert_num_to_string/main.cpp
+++ convert_num_to_string/main.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
+// Array containing the value of numbers at units place. Using array and not trying to include any types from STL.
+
+const std::string ARRAY_OF_VALUES[10]  = {
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+};
 
 long getLongInput(void) {
     std::cout << "Please enter a number between 0 and 2 billion " << std::endl;
@@ -7,13 +21,19 @@ long getLongInput(void) {
     return static_cast<long>(input);
 }
 
+// Function to check the length of the number/ string
+
+int lengthOfString(std::string input){
+   return input.size();
+}
+
 // Function to calculate number of characters and f
 
 int main(void){
     long input = getLongInput();
     std::string number_to_string = std::to_string(input);
 
-
-
+    // Checking the length of the string for demo purposes
+    std::cout << "The length of the string is " << lengthOfString(number_to_string) << std::endl;
     return 0;
 }
